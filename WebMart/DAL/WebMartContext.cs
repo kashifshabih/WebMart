69c7bd933fd14ed94194dbe4ee8275aee823105a
kashifshabih <kashif.shabih@hotmail.com>
2015-10-08 11:45:57 -0500

    DataContext and Initializer Commit

---
+++ WebMart/DAL/WebMartContext.cs
@@ -0,0 +1,22 @@
+﻿using WebMart.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace WebMart.DAL
+{
+    public class WebMartContext : DbContext
+    {
+
+        public WebMartContext()
+            : base("WebMartContext")
+        {
+        }
+        
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
