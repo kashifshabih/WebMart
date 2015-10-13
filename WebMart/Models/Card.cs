d1d61f628b006c6c5afa1a0d8d30ceb696a2ca42
ishal <timibishal@gmail.com>
2015-10-13 11:13:34 -0400

    vaidation in classes

--- WebMart/Models/Card.cs
+++ WebMart/Models/Card.cs
@@ -13,10 +13,13 @@ namespace WebMart.Models
         [Required]
         public string Name { get; set; }
         [Required]
+        [Display(Name = "Credit Card Number")]
         public BigInteger CreditCardNumber { get; set; }
         [Required]
+        [Display(Name = "Security Code")]
         public int SecurityCode { get; set; }
         [Required]
+        [Display(Name = "Expiry Date")]
         public DateTime ValidTill_DateTime { get; set; }
 
         public virtual User User { get; set; }
--- WebMart/Models/User.cs
+++ WebMart/Models/User.cs
@@ -9,16 +9,28 @@ namespace WebMart.Models
     {
         public int ID { get; set; }
         [Required]
+        [RegularExpression(@"[a-zA-Z]+", ErrorMessage = "Please enter correct name")]
+        [StringLength(100, MinimumLength = 1)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
         [Required]
+        [RegularExpression(@"[a-zA-Z]+", ErrorMessage = "Please enter correct name")]
+        [StringLength(100, MinimumLength = 1)]
+        [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
         [Required]
+        [StringLength(100, MinimumLength = 1)]
+        [Display(Name = "User Name")]
         public string UserName { get; set; }
         [Required]
+        [StringLength(100, MinimumLength = 6)]
         public string Password { get; set; }
         [Required]
+        [RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}", ErrorMessage = "Please enter correct email")]
         public string Email { get; set; }
         [Required]
+        [RegularExpression(@"[1-9][0-9]{9}", ErrorMessage = "Please enter correct phone number")]
+        [Display(Name = "Phone Number")]
         public int PhoneNumber { get; set; }
         [Required]
         public Address Address { get; set; }
