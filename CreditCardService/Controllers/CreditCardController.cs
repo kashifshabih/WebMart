d9c6d37d3e438806904d33b219c4c65ab9a6c52d
ishal <timibishal@gmail.com>
2015-10-14 11:06:57 -0400

    simple encrypt decrypt

--- CreditCardService/Controllers/CreditCardController.cs
+++ CreditCardService/Controllers/CreditCardController.cs
@@ -14,6 +14,8 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Diagnostics;
 using System.IO;
+using System.Configuration;
+using System.Web.Configuration;
 
 namespace CreditCardService.Controllers
 {
@@ -83,8 +85,10 @@ namespace CreditCardService.Controllers
             //{
                 //return BadRequest(ModelState);
             //}                                 
-
-            bool IsValid = db.CreditCards.Count(e => GetCrypt(e.CreditCardNumber) == creditCard.CreditCardNumber && GetCrypt(e.CreditCardPin) == creditCard.CreditCardPin) > 0;
+            
+            string decryptedCreditCardNumber = GetDecrypt(creditCard.CreditCardNumber);
+            string decryptedCreditCardPin = GetDecrypt(creditCard.CreditCardPin);
+            bool IsValid = db.CreditCards.Count(e => (e.CreditCardNumber == decryptedCreditCardNumber) && (e.CreditCardPin == decryptedCreditCardPin)) > 0;
             
             if (IsValid)
             {
@@ -96,6 +100,8 @@ namespace CreditCardService.Controllers
             { 
                 //send back false
             }
+
+          //  return IsValid;
 
             return IsValid;
 
@@ -135,15 +141,37 @@ namespace CreditCardService.Controllers
             return db.CreditCards.Count(e => e.Id == id) > 0;
         }
 
-        public static string GetCrypt(string text)
+        public static string StringToBinary(string data)
         {
-            return text;
-            /*string hash = "";
-            MD5 alg = MD5.Create();
-            byte[] toEncryptArray = UTF8Encoding.UTF8.GetBytes(text);            
-            byte[] result = alg.ComputeHash(toEncryptArray); 
-            hash = Encoding.UTF8.GetString(result);            
-            return hash;*/
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in data.ToCharArray())
+            {
+                sb.Append(Convert.ToString(c, 2).PadLeft(8, '0'));
+            }
+            return sb.ToString();
+        }
+
+        public static string BinaryToString(string data)
+        {
+            List<Byte> byteList = new List<Byte>();
+
+            for (int i = 0; i < data.Length; i += 8)
+            {
+                byteList.Add(Convert.ToByte(data.Substring(i, 8), 2));
+            }
+            return Encoding.ASCII.GetString(byteList.ToArray());
+        }
+
+        public static string GetDecrypt(string ciphertext)
+        {
+            Configuration config = WebConfigurationManager.OpenWebConfiguration("~/");
+            KeyValueConfigurationElement Appsetting = config.AppSettings.Settings["sequence"];            
+            var key = StringToBinary(Appsetting.Value);
+            var xor = "";
+            for (var i = 0; i < ciphertext.Length; ++i)
+                xor += ciphertext[i] ^ key[i % key.Length];                                  
+            return BinaryToString(xor);                                           
         }
     }
 }
